5e8356fed4b81e061f140da4eebba1a858308bf4
Bhaslamb <baskercarrer@gmail.com>
2022-07-24 12:57:42 +0200

    added chai exclude and deep equal module

--- src/stepdefs/api/Steps.ts
+++ src/stepdefs/api/Steps.ts
@@ -1,6 +1,11 @@
 import { Given, Then, When } from '@cucumber/cucumber';
+import chai from 'chai';
+import chaiExclude from 'chai-exclude';
+import deepEqualAnyOrder from 'deep-equal-in-any-order';
 import { expect } from '@playwright/test';
 
+chai.use(chaiExclude);
+chai.use(deepEqualAnyOrder);
 Given('the parabank rest api', function () {
   console.log('rest api');
 });
